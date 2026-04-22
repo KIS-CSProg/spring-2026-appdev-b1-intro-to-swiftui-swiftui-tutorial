679504777d64cb300269011b552bb3ecb7b971af
Joyce Pereira <joyce.pereira@kis.or.kr>
2026-04-23 08:59:55 +0900

    Create Xcode ChatPrototype project

---
+++ JPereira_ChatPrototype/JPereira_ChatPrototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPereira_ChatPrototype
+//
+//  Created by Joyce Pereira on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JPereira_ChatPrototype/JPereira_ChatPrototype/JPereira_ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPereira_ChatPrototypeApp.swift
+//  JPereira_ChatPrototype
+//
+//  Created by Joyce Pereira on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct JPereira_ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
